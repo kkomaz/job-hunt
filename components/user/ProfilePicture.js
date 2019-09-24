57014638845d8a689f48ab4fcd2605289731a1fe
Alex Lee <alex@uniteus.com>
2019-09-24 01:27:37 -0400

    Edit Profile pic

--- components/user/ProfilePicture.js
+++ components/user/ProfilePicture.js
@@ -1,6 +1,24 @@
+import { useEffect, useState } from 'react' 
+import {
+  Button,
+} from 'antd'
 import _ from 'lodash'
+import { getConfig } from 'radiks'
 
 export default function ProfilePicture({ user }) {
+  const { userSession } = getConfig();
+  const [userData, setUserData] = useState({})
+  
+  useEffect(() => {
+    if (userSession.isUserSignedIn()) {
+      setUserData(userSession.loadUserData())
+    }
+  }, [])
+
+  const changeProfilePicture = () => {
+    window.open('https://browser.blockstack.org/profiles', '_blank');
+  }
+
   return (
     <div className="image-container">
       <img
@@ -12,11 +30,23 @@ export default function ProfilePicture({ user }) {
         }}
       />
 
+      {
+        userData.username === user.username &&
+        <Button
+          type="primary"
+          className="mt-one"
+          onClick={changeProfilePicture}
+        >
+          Edit Profile Picture
+        </Button>
+      }
       <style jsx>
         {`
           .image-container {
             display: flex;
             justify-content: center;
+            flex-direction: column;
+            align-items: center;
           }
         `}
       </style>
